ee065aed53ee368abcf859a03048b51b162287c1
Fatimah2014 <80806004+Fatimah2014@users.noreply.github.com>
2021-09-01 16:12:06 -0400

    trying to fix bug

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -21,23 +21,7 @@ router.post("/workouts", (req,res) => {
     })
   })
 
-  router.put("/workouts/:id", (req,res) => {
-    Workout.findOneAndUpdate(
-        {_id:req.params.id
-        },
-        {
-            $inc:{totalDuration: req.body.duration},
-           $push:{exercises: req.body} 
-
-        }
-    )
-    .then(results=>{
-        res.status(200).json(results)
-    })  
-    .catch(err=>{
-        res.status(400).json({message:"no workouts found"})
-    })
-  })
+ 
   
   
 
